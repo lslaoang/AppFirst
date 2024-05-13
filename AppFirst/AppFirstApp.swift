2a55fecddffb5a9fcc8232c21a59e36b81c69283
lslaoang <laurellaoang@gmail.com>
2024-05-13 21:58:04 +0200

    Initial Commit

---
+++ AppFirst/AppFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFirstApp.swift
+//  AppFirst
+//
+//  Created by Laurel Laoang, Jr on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AppFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
